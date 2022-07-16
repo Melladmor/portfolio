4cd475831fd4640f74a299de77d48feab5df9813
Mellad Mor <melladmor@gmail.com>
2022-07-16 15:58:28 +0300

    update2

--- src/component/portfolio/Portfolio.js
+++ src/component/portfolio/Portfolio.js
@@ -1,21 +1,37 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import './portfolio.css';
 import { DATAEN } from '../../data/dataEn';
 import AOS from 'aos';
+import crud from '../../assets/crud.png';
+import employee from '../../assets/employe.png';
+import quiz from '../../assets/quiz.png';
+import shop from '../../assets/shopcart.png';
+import toDo from '../../assets/todoapp.png';
+import meme from '../../assets/memegen.png';
+import tenzic from '../../assets/tenzic.png';
+import code from '../../assets/editor.png';
+import radio from '../../assets/radio.png';
+
 import 'aos/dist/aos.css'; // You can also use <link> for styles
 // ..
 AOS.init();
+
 const Portfolio = () => {
 
     const [data , setData] = useState(DATAEN);
 
+    const img = [crud ,employee , quiz , shop , toDo , meme , tenzic , code , radio];
+
+    var count =-1;
+
 
 
     const projects = data.map((el)=>{
+        count ++;
         return(
             <article className='portfolio__item' key={el.id} data-aos="zoom-in" data-aos-duration="2000">
                     <div className='portfolio__item-image'>
-                    <img src={el.img} alt={el.name}/>
+                    <img src={img[count]} alt={el.name}/>
                     </div>
                     <h3>{el.name}</h3>
                     <h4>{el.description}</h4>
@@ -24,6 +40,8 @@ const Portfolio = () => {
             </article>
         )
     })
+
+
     return (
         <section id='portfolio'>
             <h5>My Recent Work</h5>
